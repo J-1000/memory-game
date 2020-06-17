dff919d9947c796656b56e139908e696a62d9c80
Anna Weingart <annaraewein@gmail.com>
2020-06-17 17:39:38 +0200

    Fixed shuffle, added timer, added game over, added play again button

--- scripts.js
+++ scripts.js
@@ -24,30 +24,74 @@ function flipCard() {
                 //it's a match!
                 firstCard.removeEventListener('click', flipCard);
                 secondCard.removeEventListener('click', flipCard);
-                resetBoard();
+                //resetBoard();
             }   else {
                 //not a match
                 lockBoard = true;
                 setTimeout(() => {
                     firstCard.classList.remove('flip');
                     secondCard.classList.remove('flip');
-                    resetBoard();
+                    [hasFlippedCard, lockBoard] = [false, false];
+                    [firstCard, secondCard] = [null, null];
                 }, 1500);
             }
-        function resetBoard() {
-            [hasFlippedCard, lockBoard] = [false, false];
-            [firstCard, secondCard] = [null, null];
-        } 
-        (function shuffle() {
-            cards.forEach(card => {
-                let randomPos = math.floor(math.random() * 12);
-                card.style.order = randomPos;
-            })(); 
-        }
 
-        )}
+        
+    }
 
 
 }
+function resetBoard() {
+    counter = 0;
+    document.querySelector(".memory-game").style.visibility = "visible";
+    document.querySelector(".btn").style.visibility = "hidden";
+    cards.forEach(function(card) {
+        card.classList.remove('flip');
+        card.addEventListener('click', flipCard)
+    })
+    startTimer();
+    shuffle();
+} 
+
 
 cards.forEach(card => card.addEventListener('click', flipCard)); 
+
+var counter = 0;
+var timeLeft = 10;
+
+function startTimer() {
+    var timer = select('#timer');
+    timer.html(timeLeft - counter);
+
+
+var interval = setInterval(timeIt, 1000);
+
+    function timeIt() {
+        counter++;
+        timer.html(timeLeft - counter);
+        if (counter == timeLeft) {
+            clearInterval(interval);
+            timer.html("Game Over")
+
+            document.querySelector(".memory-game").style.visibility = "hidden";
+            let button = document.querySelector(".btn");
+            button.style.visibility = "visible";
+            button.addEventListener("click", resetBoard)
+            //alert('boom');
+        }
+
+
+    }
+}
+
+function setup() {
+    noCanvas();
+    startTimer();
+}
+function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    }); 
+}
+//shuffle();
